ab4dfbc7ced9cc9569e6733174b557a7d946e199
steve-ux <exe.universal@gmail.com>
2022-06-15 22:29:35 -0300

    se cambió el video y el whatsapp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import Videoloop from "../assets/cars.mp4";
+import Videoloop from "../assets/F1.mp4";
 import Logotipo from "../assets/logo-completo-white.png";
 
 function Home() {
@@ -10,13 +10,13 @@ function Home() {
         </video>
         <div className="containerElements">
         <span className="spanHome">
-        <h1 className="titleInicio"><img src={Logotipo} width="500" alt="Logotipo de Greco"/></h1>
+        <h1 className="titleInicio"><img src={Logotipo} className="logo-header" width="500" alt="Logotipo de Greco"/></h1>
         </span>
         <p className="parrafoHome">
             EL MEJOR RECTIFICADOR DE TAPAS DE CILINDRO.
           </p>
           <div class="center">
-        <a href="https://api.whatsapp.com/send?phone=5492612121844&text=Hola,%20visité%20su%20web%20y%20quisiera%20conocer%20más%20sobre%20sus%20productos" target="_blank" rel="noreferrer"><span data-attr="WHATS">WHATS</span><span data-attr="APPS">APPS</span></a>
+        <a href="https://api.whatsapp.com/send?phone=5492616963854&text=Hola!%20Visité%20su%20web%20y%20quisiera%20conocer%20más%20sobre%20sus%20productos" target="_blank" rel="noreferrer"><span data-attr="WHATS">WHATS</span><span data-attr="APPS">APPS</span></a>
     </div>
           </div>
           
