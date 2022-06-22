b89e64fc5e74210a4343b00f85254aa61a5647b1
steve-ux <exe.universal@gmail.com>
2022-06-21 23:34:41 -0300

    se actualizó el correo de consultas

--- src/components/FormularioContacto.js
+++ src/components/FormularioContacto.js
@@ -1,37 +1,3 @@
-// function Formulario() {
-//     return (
-//       <>
-//       <div className="formularioContainer2">
-//         <div className="content">
-//         <form action="https://formsubmit.co/grecohnostc@hotmail.com" method="POST">
-//         <label for="full_name">Nombre completo:</label>
-//          <input required id="full_name" name="Nombre Completo" 
-//          type="text"/> 
-         
-//          <label for="email">Email:</label>
-//          <input required id="email" name="Email" 
-//          type="email"/>
-  
-        
-//          <label for="message">Mensaje:</label>
-//          <textarea required id="message" name="Mensaje" 
-//          type="textarea" minLength="10" title="Mensaje mínimo de 10 caracteres." className="inputMsj"/>
-  
-//         {/* <input type="hidden" name="_next" value="https://.com/Enviado"/> Poner URL del dominio */}
-//         <input type="hidden" name="_subject" value="Nueva consulta desde la web!"/>
-//         <input type="hidden" name="_captcha" value="false"/>
-         
-  
-//         <button className="btnSend" type="submit" value="Send">ENVIAR</button>
-//         </form>
-//         </div>
-//         </div>
-//         </>
-      
-//     );
-//   }
-  
-//   export default Formulario;
 import "../form.scss";
 import CilindroFalcon from "../assets/tapa.jpeg";
 
@@ -45,7 +11,7 @@ import CilindroFalcon from "../assets/tapa.jpeg";
       <h2 class="animation a1">Te respondemos a la brevedad</h2>
       <h4 class="animation a2">* Campos obligatorios.</h4>
        </div>
-    <form class="formu" action="https://formsubmit.co/exe.universal@gmail.com" method="POST">
+    <form class="formu" action="https://formsubmit.co/grecohnostc@hotmail.com" method="POST">
       <input required id="full_name" type="text" class="form-field animation a4" placeholder="* Nombre"/>
       <input required id="email" type="email" class="form-field animation a3" placeholder="* Correo"/>
       <textarea required id="message" name="Mensaje" type="textarea" minLength="10" title="Mensaje mínimo de 10 caracteres." className="inputMsj" class="form-field animation a3" placeholder="* Tu consulta"/>
